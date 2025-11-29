8f06b9cd8379644cee5b3ec9e6b09f2e8bb78dd3
Juan-Wykner <juanwykner@gmail.com>
2025-11-29 16:18:24 -0300

    Static: remover core/static de STATICFILES_DIRS para evitar duplicatas (AppDirectoriesFinder já coleta)

--- config/settings.py
+++ config/settings.py
@@ -73,10 +73,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core', 'static'),
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = []
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Enable WhiteNoise to serve static files efficiently
